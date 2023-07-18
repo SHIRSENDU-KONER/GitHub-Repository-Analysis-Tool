00270ebecd49d05f50d7a571dcadad192aff875d
SHIRSENDU KONER <56688339+SHIRSENDU-KONER@users.noreply.github.com>
2023-07-18 20:07:37 +0530

    main file added

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main(username, git_access_token):
         try:
             # STEP - 1 : FETCHING THE REPO NAMES
             x = time.time()
-            user, repo_names, repo_descriptions = display_repo_names_and_url(username, git_access_token)
+            user, repo_names, repo_descriptions, repo_urls = display_repo_names_and_url(username, git_access_token)
             y = time.time()
             st.write(f"Repo Names Has been fetched . Time taken for fetching repo names : {y - x} sec")
             print('#' * 50)
@@ -47,11 +47,15 @@ def main(username, git_access_token):
             # STEP - 3 : CONVERT THE DATA INTO A STRUCTURED FORMAT
             st.write("Proceeding to convert fetched data into a csv file...")
             x = time.time()
-            df = pd.DataFrame(columns=["repo", "file", "loc", "lloc", "sloc", "blank_lines", "comments",
+            df = pd.DataFrame(columns=["repo", "file","url", "loc", "lloc", "sloc", "blank_lines", "comments",
                                        "multiline_comments", "single_comments", "cyclomatic_complexity",
                                        "halstead_report"])
             data_rows = []
+            index_value = -1
             for rep, files in all_repo_details.items():
+                index_value += 1
+                desription = repo_descriptions.get(f'{rep}', '')
+                repo_url = repo_urls[index_value]
                 for file, file_info in files.items():
                     loc = file_info.get('loc', '')
                     lloc = file_info.get('lloc', '')
@@ -66,6 +70,8 @@ def main(username, git_access_token):
                     # Append a row to the DataFrame
                     data_rows.append({
                         "repo": rep,
+                        "description":desription,
+                        "url": repo_url,
                         "file": file,
                         "loc": loc,
                         "lloc": lloc,
@@ -97,6 +103,7 @@ if __name__ == "__main__":
 
     st.title('Github Automated Analysis | Most Technically Complex And Challenging Repo')
     github_url = st.text_input("Please Enter The GitHub User's URL or Username For Analysis")
+    # git_access_token = st.text_input("Please provide the Github API key", type="password")
     openai_api_key = st.text_input("Please provide the OpenAI API key", type="password")
     if len(github_url.split('/')) > 1:
         username = github_url.split('/')[-1]
